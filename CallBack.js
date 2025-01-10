e67ffacc244931d2a1d6c4ba15b3f2f578eebcb4
Raiyan Ahmed <raiyan00719@gmail.com>
2025-01-10 17:07:50 +0600

    Add files via upload

---
+++ CallBack.js
@@ -0,0 +1,203 @@
+function one(callback, v1, v2, v3, v4, v5) {
+    setTimeout(() => {
+      let value = v1 + v2 + v3 + v4 + v5;
+      console.log(`one number is: ${value}`);
+      callback(value);
+    }, 300);
+  }
+  
+  function two(callback, v1, v2, v3, v4, v5, v6, v7) {
+    setTimeout(() => {
+      let value = v1 + v2 + v3 + v4 + v5 + v6 + v7;
+      console.log(`two number is: ${value}`);
+      callback(value);
+    }, 600);
+  }
+  
+  function three(callback, v1, v2, v3) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3);
+      console.log(`three number is: ${value}`);
+      callback(value);
+    }, 200);
+  }
+  
+  function four(callback, v1, v2, v3, v4, v5, v6, v7, v8) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8);
+      console.log(`four number is: ${value}`);
+      callback(value);
+    }, 500);
+  }
+  
+  function five(callback, v1, v2, v3, v4, v5) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5);
+      console.log(`five number is: ${value}`);
+      callback(value);
+    }, 100);
+  }
+  
+  function six(callback, v1, v2, v3, v4, v5, v6) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6);
+      console.log(`six number is: ${value}`);
+      callback(value);
+    }, 300);
+  }
+  
+  function seven(callback, v1, v2, v3, v4, v5, v6, v7) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7);
+      console.log(`seven number is: ${value}`);
+      callback(value);
+    }, 400);
+  }
+  
+  function eight(callback, v1, v2, v3, v4, v5, v6, v7, v8) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8);
+      console.log(`eight number is: ${value}`);
+      callback(value);
+    }, 600);
+  }
+  
+  function nine(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9);
+      console.log(`nine number is: ${value}`);
+      callback(value);
+    }, 700);
+  }
+  
+  function ten(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10);
+      console.log(`ten number is: ${value}`);
+      callback(value);
+    }, 800);
+  }
+  
+  function eleven(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11);
+      console.log(`eleven number is: ${value}`);
+      callback(value);
+    }, 900);
+  }
+  
+  function twelve(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12);
+      console.log(`twelve number is: ${value}`);
+      callback(value);
+    }, 1000);
+  }
+  
+  function thirteen(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12 + v13);
+      console.log(`thirteen number is: ${value}`);
+      callback(value);
+    }, 1100);
+  }
+  
+  function fourteen(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12 + v13 + v14);
+      console.log(`fourteen number is: ${value}`);
+      callback(value);
+    }, 1200);
+  }
+  
+  function fifteen(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12 + v13 + v14 + v15);
+      console.log(`fifteen number is: ${value}`);
+      callback(value);
+    }, 1300);
+  }
+  
+  function sixteen(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12 + v13 + v14 + v15 + v16);
+      console.log(`sixteen number is: ${value}`);
+      callback(value);
+    }, 1400);
+  }
+  
+  function seventeen(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12 + v13 + v14 + v15 + v16 + v17);
+      console.log(`seventeen number is: ${value}`);
+      callback(value);
+    }, 1500);
+  }
+  
+  function eighteen(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12 + v13 + v14 + v15 + v16 + v17 + v18);
+      console.log(`eighteen number is: ${value}`);
+      callback(value);
+    }, 1600);
+  }
+  
+  function nineteen(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12 + v13 + v14 + v15 + v16 + v17 + v18 + v19);
+      console.log(`nineteen number is: ${value}`);
+      callback(value);
+    }, 1700);
+  }
+
+  function twenty(callback, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20) {
+    setTimeout(() => {
+      let value = parseInt(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12 + v13 + v14 + v15 + v16 + v17 + v18 + v19 + v20);
+      console.log(`twenty number is: ${value}`);
+      callback(value);
+    }, 1800);
+  }
+  
+  // Callback Hell Implementation
+  one((value1) => {
+    two((value2) => {
+      three((value3) => {
+        four((value4) => {
+          five((value5) => {
+            six((value6) => {
+              seven((value7) => {
+                eight((value8) => {
+                  nine((value9) => {
+                    ten((value10) => {
+                      eleven((value11) => {
+                        twelve((value12) => {
+                          thirteen((value13) => {
+                            fourteen((value14) => {
+                              fifteen((value15) => {
+                                sixteen((value16) => {
+                                  seventeen((value17) => {
+                                    eighteen((value18) => {
+                                      nineteen((value19) => {
+                                        twenty((value20) => {
+                                          console.log("All done! Callback Hell completed. 🎉");
+                                        }, value19, value18, value17, value16, value15, value14, value13, value12, value11, value10, value9, value8, value7, value6, value5, value4, value3, value2, value1);
+                                      }, value18, value17, value16, value15, value14, value13, value12, value11, value10, value9, value8, value7, value6, value5, value4, value3, value2);
+                                    }, value17, value16, value15, value14, value13, value12, value11, value10, value9, value8, value7, value6, value5, value4, value3);
+                                  }, value16, value15, value14, value13, value12, value11, value10, value9, value8, value7, value6, value5, value4);
+                                }, value15, value14, value13, value12, value11, value10, value9, value8, value7, value6, value5);
+                              }, value14, value13, value12, value11, value10, value9, value8, value7, value6);
+                            }, value13, value12, value11, value10, value9, value8, value7, value6, value5);
+                          }, value12, value11, value10, value9, value8, value7, value6, value5, value4);
+                        }, value11, value10, value9, value8, value7, value6, value5, value4, value3);
+                      }, value10, value9, value8, value7, value6, value5, value4, value3, value2);
+                    }, value9, value8, value7, value6, value5, value4, value3, value2, value1);
+                  }, value8, value7, value6, value5, value4, value3, value2, value1);
+                }, value7, value6, value5, value4, value3, value2, value1);
+              }, value6, value5, value4, value3, value2, value1);
+            }, value5, value4, value3, value2, value1);
+          }, value4, value3, value2, value1);
+        }, value3, value2, value1);
+      }, value2, value1);
+    }, value1);
+  });
+  
